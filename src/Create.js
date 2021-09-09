29a8e9f8a1684c7022780eb999be9bd63c60a8fa
AV <vihrogonov@protonmail.com>
2021-09-09 17:18:55 +0100

    Submit Events

--- src/Create.js
+++ src/Create.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // create blog object
+    const blog = { title, body, author };
+    console.log(blog);
+  };
+
   return (
     <div>
       <div className='create'>
         <h2>Add a New Blog</h2>
+        <form onSubmit={handleSubmit}>
+          <label>Blog title:</label>
+          <input
+            type='text'
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Blog body:</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <label>Blog author</label>
+          <select value={author} onChange={(e) => e.target.val}>
+            <option value='mario'>mario</option>
+            <option value='yoshi'>yoshi</option>
+          </select>
+          <button>Add Blog</button>
+        </form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Create
+export default Create;
